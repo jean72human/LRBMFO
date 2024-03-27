75cb5d3df753605e301a1382e2a4c6ecec0b7415
Gbetondji DOVONON <gdovonon@catbells.cs.ucl.ac.uk>
2024-03-27 15:48:47 +0000

    add device changes to the scripts

--- exp_hartmann_mfmes_miso.sh
+++ exp_hartmann_mfmes_miso.sh
@@ -10,15 +10,16 @@ BUDGET=100
 SEED=72
 RESULTFOLDER="results_exp_hartmann_mfkg_miso"
 MOGP="miso"
-NAME="exp_hartmann_mfkg_miso"
+NAME="exp_hartmann_mfmes_miso"
 VERBOSE=1
+DEVICE='cuda:0'
 
 
 echo -e "FUNCTIONS=$EXP\nCOSTS=$COSTS\nMETHODS=$METHODS\nCONDITION_VAR=$COND_VAR\nCONDITION_IG=$COND_IG\nN_REPS=$N_REP\nBUDGET=$BUDGET\nSEED=$SEED" > "$RESULTFOLDER/config_$NAME.txt"
 if [[ $EXP == *"Multiple"* ]]
     then
         # cost parameter has no impact in multiple case, it is hardcoded to 0.2 for all AIS and 1.0 for target!
-        python3 main_multiple_debug.py -n $N_REP -b $BUDGET -s "$RESULTFOLDER/$NAME" -se $SEED -m $METHODS -v $VERBOSE -cv $COND_VAR -ci $COND_IG -co $COSTS -e $EXP -j $MOGP
+        python3 main_multiple_debug.py -n $N_REP -b $BUDGET -s "$RESULTFOLDER/$NAME" -se $SEED -m $METHODS -v $VERBOSE -cv $COND_VAR -ci $COND_IG -co $COSTS -e $EXP -j $MOGP -d $DEVICE
     else
-        python3 main.py -n $N_REP -b $BUDGET -s "$RESULTFOLDER/$NAME" -se $SEED -m $METHODS -v $VERBOSE -cv $COND_VAR -ci $COND_IG -co $COSTS -e $EXP -j $MOGP
+        python3 main.py -n $N_REP -b $BUDGET -s "$RESULTFOLDER/$NAME" -se $SEED -m $METHODS -v $VERBOSE -cv $COND_VAR -ci $COND_IG -co $COSTS -e $EXP -j $MOGP -d $DEVICE
 fi
